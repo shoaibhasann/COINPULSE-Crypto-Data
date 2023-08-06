a6d21f83bebbb3fb050ff471c9eb27de4e2ab343
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-07 02:18:27 +0530

    create loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import  '../styles/Loader.css';
+
+function Loader() {
+  return (
+      <div className="banter-loader">
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+      </div>
+  );
+}
+
+export default Loader
