e8ef2494b064f3a36fc7ffd4f375daccbe450ae1
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-06 20:21:14 +0530

    Home page created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Join from "../components/Join";
+import Market from "../components/Market";
+import Why from "../components/Why";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Market />
+      <Why />
+      <Join />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
