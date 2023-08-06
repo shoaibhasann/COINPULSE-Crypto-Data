0356af76f8d3395ddc29cdb01c5a6813c50dab37
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-07 02:19:11 +0530

    create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Join from "../components/Join";
-import Market from "../components/Market";
 import Why from "../components/Why";
 
 function Home() {
   return (
     <>
       <Hero />
-      <Market />
       <Why />
       <Join />
       <Footer />
