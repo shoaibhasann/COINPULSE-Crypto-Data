a15db6f60cbc242a5b7e3f75d039ad9e1f9aa89d
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-08 02:06:44 +0530

    fix some issues

--- src/components/Exchangecard.jsx
+++ src/components/Exchangecard.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 function Exchangecard({name, url, price, rank, image, percent}) {
   return (
-    <Link to={url} className="grid grid-cols-3 place-items-center px-8 py-6 border-b cursor-pointer border-white hover:bg-[#200840]">
+    <Link
+      to={url}
+      className="grid grid-cols-3 place-items-center px-8 py-6 border-b cursor-pointer border-white hover:bg-[#200840]"
+    >
       <div className="flex items-center gap-5">
         <img className="w-10 h-10" src={image} alt="logo" />
         <div className="title text-white font-medium text-xl">{name}</div>
       </div>
       <div className="text-white font-medium text-xl">{rank}</div>
-      <div className="text-white font-medium text-xl">{"$ " + price}</div>
+      <div className="text-white font-medium text-xl">{"₹ " + price}</div>
     </Link>
   );
 }
