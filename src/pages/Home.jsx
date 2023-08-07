315686f7a445765784a036186d54004c7996c162
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-08 02:08:16 +0530

    change home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Join from "../components/Join";
-import Why from "../components/Why";
 
 function Home() {
   return (
     <>
       <Hero />
-      <Why />
       <Join />
       <Footer />
     </>
