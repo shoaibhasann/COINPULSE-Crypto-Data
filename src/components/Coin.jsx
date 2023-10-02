fa39a2f1eb72e8dda4fb883cac181459ccfc8964
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-02 17:57:27 +0530

    fix import statements

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Loader from "./Loader";
-import CoinCard from "./CoinCard";
+import Loader from "./Loader.jsx";
+import CoinCard from "./CoinCard.jsx";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 export function numberWithCommas(x) {
